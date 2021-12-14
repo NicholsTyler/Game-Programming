18e34641b4d287943d6d4380f2c646d4d6614bdd
Tyler Nichols <83674440+NicholsTyler@users.noreply.github.com>
2021-12-14 00:07:45 -0500

    Create Data.cs

---
+++ Patterns/Design_Patterns/Flyweight/Data.cs
@@ -0,0 +1,25 @@
+
+/// <summary> Data that can be shared across all objects </summary>
+public class Data
+{
+    double number1 = 5d;
+    double number2 = 5d;
+    double number3 = 5d;
+    double number4 = 5d;
+    double number5 = 5d;
+    double number6 = 5d;
+    double number7 = 5d;
+    double number8 = 5d;
+    double number9 = 5d;
+    double number10 = 5d;
+    double number11 = 5d;
+    double number12 = 5d;
+    double number13 = 5d;
+    double number14 = 5d;
+    double number15 = 5d;
+    double number16 = 5d;
+    double number17 = 5d;
+    double number18 = 5d;
+    double number19 = 5d;
+    double number20 = 5d;
+}
