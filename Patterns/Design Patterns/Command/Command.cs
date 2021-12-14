056776d4321e471bbc24c3afe24091f736b6a5b3
Tyler Nichols <83674440+NicholsTyler@users.noreply.github.com>
2021-12-13 19:19:34 -0500

    Update Command.cs

--- Patterns/Design Patterns/Command/Command.cs
+++ Patterns/Design Patterns/Command/Command.cs
@@ -1,10 +1,3 @@
-#region Namespaces
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-#endregion
-
-
 /// <summary> Base Class for implementing the Command Design Pattern </summary>
 public abstract class Command
 {
