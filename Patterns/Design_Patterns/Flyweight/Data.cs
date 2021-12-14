d92ccd2a70f859962a7b26e363dafa0d15d097db
Tyler Nichols <83674440+NicholsTyler@users.noreply.github.com>
2021-12-14 01:25:42 -0500

    Update Data.cs

--- Patterns/Design_Patterns/Flyweight/Data.cs
+++ Patterns/Design_Patterns/Flyweight/Data.cs
@@ -1,25 +1,29 @@
+#region Namespaces
+using UnityEngine;
+#endregion
 
 /// <summary> Data that can be shared across all objects </summary>
-public class Data
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Data", order = 1)]
+public class Data : ScriptableObject
 {
-    double number1 = 5d;
-    double number2 = 5d;
-    double number3 = 5d;
-    double number4 = 5d;
-    double number5 = 5d;
-    double number6 = 5d;
-    double number7 = 5d;
-    double number8 = 5d;
-    double number9 = 5d;
-    double number10 = 5d;
-    double number11 = 5d;
-    double number12 = 5d;
-    double number13 = 5d;
-    double number14 = 5d;
-    double number15 = 5d;
-    double number16 = 5d;
-    double number17 = 5d;
-    double number18 = 5d;
-    double number19 = 5d;
-    double number20 = 5d;
+    public double number1 = 5d;
+    public double number2 = 5d;
+    public double number3 = 5d;
+    public double number4 = 5d;
+    public double number5 = 5d;
+    public double number6 = 5d;
+    public double number7 = 5d;
+    public double number8 = 5d;
+    public double number9 = 5d;
+    public double number10 = 5d;
+    public double number11 = 5d;
+    public double number12 = 5d;
+    public double number13 = 5d;
+    public double number14 = 5d;
+    public double number15 = 5d;
+    public double number16 = 5d;
+    public double number17 = 5d;
+    public double number18 = 5d;
+    public double number19 = 5d;
+    public double number20 = 5d;
 }
