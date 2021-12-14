0c508917ab387ea994d77c3aff8c11d5250610f3
Tyler Nichols <83674440+NicholsTyler@users.noreply.github.com>
2021-12-14 00:06:58 -0500

    Update and rename GameController.cs to CommandController.cs

--- Patterns/Design_Patterns/Command/CommandController.cs
+++ Patterns/Design_Patterns/Command/CommandController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 #endregion
 
-public class GameController : MonoBehaviour
+public class CommandController : MonoBehaviour
 {
     [Tooltip("The movable object")]
     public BasicMove moveObject;
